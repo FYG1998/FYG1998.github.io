f4237db37f38c509e68d9f3d9bb82d3817248918
naiop <36446811+naiop@users.noreply.github.com>
2021-08-30 11:29:53 +0800

    Update main.js

--- tools/main.js
+++ tools/main.js
@@ -15,7 +15,7 @@ $(function() {
 				console.log(n);
 				var sj = Math.ceil(Math.random() * 7) //随机产生数
 				console.log(sj);
-				str += "<div class='col-lg-4 tool-item'> <a href='" + n.url + "'><div class='maple-tool-item image-shadow'><span class='maple-tool-icon maple-tool-item-color" + sj + "'>" + n.ico + "</span><h3 class='maple-tool-name'>" + n.title + "</h3><span class='maple-tool-describe'>" + n.desc + "</span><div class='maple-tool-tags'><span class='maple-tool-tag' title='工具类型'>" + n.tag1 + "</span><span class='maple-tool-tag' title='工具说明'>" + n.tag1 + "</span></div><div style='overflow: hidden;'><span class='maple-tool-in' title='点击打开工具 '> Open</span></div></div></a></div>";
+				str += "<div class='col-lg-4 tool-item'> <a href='" + n.url + "'><div class='maple-tool-item image-shadow'><span class='maple-tool-icon maple-tool-item-color" + sj + "'>" + n.ico + "</span><h3 class='maple-tool-name'>" + n.title + "</h3><span class='maple-tool-describe'>" + n.desc + "</span><div class='maple-tool-tags'><span class='maple-tool-tag' title='工具类型'>" + n.tag1 + "</span><span class='maple-tool-tag' title='工具说明'>" + n.tag2 + "</span></div><div style='overflow: hidden;'><span class='maple-tool-in' title='点击打开工具 '> Open</span></div></div></a></div>";
 			});
 			str += "";
 			$("#test").append(str);
